9f396978bfdfa0c198e612d8b020963c04696890
= <=>
2023-10-02 12:56:43 -0300

    add

---
+++ getXmlCte/src/main/java/br/com/planck/getXmlCte/GetXmlCteApplication.java
@@ -0,0 +1,13 @@
+package br.com.planck.getXmlCte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetXmlCteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetXmlCteApplication.class, args);
+	}
+
+}
---
+++ getXmlCte/src/test/java/br/com/planck/getXmlCte/GetXmlCteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.planck.getXmlCte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetXmlCteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
